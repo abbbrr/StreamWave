eac14bf9ec69bfdd4f0ea8f6f50283900ed9a0d9
Ablaikhan Rauan <ruanablaikz45@gmail.com>
2023-09-28 03:28:43 +0600

    сделал лучше вывод данных из Firestore

--- collegeApp/ViewModel/CartViewModel.swift
+++ collegeApp/ViewModel/CartViewModel.swift
@@ -1,59 +1,45 @@
-//
-//  CartViewModel.swift
-//  collegeApp
-//
-//  Created by Рауан Аблайхан on 21.09.2023.
-//
-
 import Foundation
+import Firebase
 import FirebaseFirestore
 
 class CartViewModel:ObservableObject{
+    @Published var podcasts = [PodcastCart]()
+
+    private var db = Firestore.firestore()
     
-    func addPodcastCart(podcastCart: PodcastCart){
-        let db = Firestore.firestore()
-        
-        db.collection("podcastCart").addDocument(data: [
-            "name": podcastCart.name,
-            "image": podcastCart.image,
-            "author": podcastCart.author,
-            "information": podcastCart.information,
-            "video": podcastCart.videoUrl,
-            "raiting": podcastCart.raiting
-        ]) { (error) in
-            if let error = error {
-                print("Ошибка при добавлении карточки в Firestore: \(error)")
-            } else{
-                print("Карточка успешно добавлена в Firestore")
-            }
-        }
+    init() {
+        fetchData()
     }
     
     
-    func getPodcastCart(completion: @escaping([PodcastCart]?, Error?) -> Void){
-        let db = Firestore.firestore()
-        
-        db.collection("podcastCart").getDocuments { (querySnapshot, error) in
+    func fetchData(){
+        db.collection("PodcastCard").getDocuments {(querySnapshot, error) in
             if let error = error {
-                completion(nil, error)
-            } else {
-                var podcastCarts = [PodcastCart]()
+                print("Error fetching documents: \(error)")
+                return
+            }
+            
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.podcasts = []
+            
+            for document in documents {
+                let data = document.data()
+                let name = data["name"] as? String ?? ""
+                let image = data["image"] as? String ?? ""
+                let author = data["author"] as? String ?? ""
+                let information = data["information"] as? String ?? ""
+                let videoUrl = data["videoUrl"] as? String ?? ""
+                let audioUrl = data["audioUrl"] as? String ?? ""
+                let raiting = data["raiting"] as? Int ?? 0
                 
-                for document in querySnapshot!.documents{
-                    if let data = document.data() as? [String: Any],
-                       let name = data["name"] as? String,
-                       let image = data["image"] as? String,
-                       let author = data["author"] as? String,
-                       let information = data["information"] as? String,
-                       let video = data["videoUrl"] as? String,
-                       let raiting = data["raiting"] as? Double{
-                            
-                        let podcastCard = PodcastCart(name: name, image: image, author: author, information: information, videoUrl: video,  raiting: raiting)
-                        podcastCarts.append(podcastCard)
-                    }
-                }
-                completion(podcastCarts, nil)
+                let podcast = PodcastCart(id: .init(), name: name, image: image, author: author, information: information, videoUrl: videoUrl, audioUrl: audioUrl, raiting: raiting)
+                self.podcasts.append(podcast)
             }
         }
     }
+    
 }
